99abad26a6e7587c33e12f6980135c5ace2e119b
Alex <alex@unixfy.net>
2022-06-22 22:59:39 +0000

    Bootstrap the site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="min-h-screen">
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-8 min-h-screen">
+		<div class="flex justify-center contents-center flex-col">
+			<img src="/curvy_things.svg" alt="Curvy things" class="absolute top-0 left-0 w-64" />
+			<p class="font-display text-4xl drop-shadow-lg ml-16">👋 Hey there, I'm</p>
+			<h1
+				class="font-display text-8xl bg-gradient-to-t from-[#7BC6CC] to-[#BE93C5] bg-clip-text text-transparent font-bold drop-shadow-lg my-8 py-2 ml-16"
+			>
+				Alex Wang
+			</h1>
+			<p class="font-display text-2xl ml-16">I like cheesy taglines.</p>
+		</div>
+		<div class="flex justify-center contents-center flex-col">
+			<div class="relative">
+				<img src="/blob.svg" alt="Background blob" class="" />
+				<img
+					src="/hello.png"
+					alt="Hello!"
+					class="absolute top-[12.5%] w-[75%] h-[75%] left-[12.5%]"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="bg-gray-200 p-8">
+	<div class="grid grid-cols-1 md:grid-cols-5 gap-6">
+		<div class="col-span-2 flex">
+			<h2 class="text-4xl m-auto">
+				I am a <span class="font-bold text-5xl font-display">web developer</span>
+			</h2>
+		</div>
+		<div class="col-span-3">
+			<p class="text-xl">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tellus convallis, tincidunt
+				nisl eget, faucibus diam. Nullam ac nisi ac ante lacinia convallis. Suspendisse id lacus et
+				dui interdum tincidunt. Aenean sollicitudin diam vel suscipit auctor. Suspendisse tempus
+				rutrum nunc in pulvinar. Mauris auctor, nisi non auctor varius, diam ipsum efficitur nulla,
+				in accumsan lectus augue quis nulla. Sed ut ex libero. Sed aliquam sed massa sit amet
+				suscipit.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="py-12 px-8 bg-[#7E1F86] flex">
+    <p class="text-3xl italic text-white m-auto font-light">If passion drives you, let reason hold the reins.</p>
+</div>
+
+<div class="p-8">
+    <h1 class="font-display text-4xl font-bold text-center">Writing</h1>
+</div>
+
+<div class="p-8 bg-gray-200">
+    <h1 class="font-display text-4xl font-bold text-center my-4 -rotate-90">Stuff I've Done</h1>
+</div>
