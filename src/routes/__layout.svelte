1e7f9d9a9df51978c1942c1543edd88d8e09dbf5
Alex <alex@unixfy.net>
2022-06-22 22:03:23 +0000

    Add footer to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
     import '@fontsource/ibm-plex-sans';
     import '@fontsource/space-grotesk';
+    import Footer from '$components/Footer.svelte';
 </script>
 
 <div class="min-h-screen"><slot /></div>
+
+<Footer/>
