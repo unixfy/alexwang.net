a5494193ca86166b28df9c0fb529423eb3d3bd8d
Alex <alex@unixfy.net>
2022-07-16 19:03:53 +0000

    Add a simple Linkedin redirect url

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -26,10 +26,14 @@
 </script>
 
 <div class="text-xl font-display font-light">
-	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">Home</span>
-	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx=2">Projects</span>
-	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">LinkedIn</span>
-	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-4 ml-2" on:click="{toggleDarkMode}"
+	<a href="/"><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">Home</span></a>
+	<a href="/projects"
+		><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx=2">Projects</span></a
+	>
+	<a href="/linkedin"
+		><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">LinkedIn</span></a
+	>
+	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-4 ml-2 hover:cursor-pointer" on:click={toggleDarkMode}
 		><i class="fa-solid fa-sun" /></span
 	>
 </div>
---
+++ src/routes/linkedin.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+	export async function load() {
+		return {
+			status: 302,
+			redirect: 'https://linkedin.com/in/alex-y-wang'
+		};
+	}
+</script>
