2a3411d9a44fa97a3c2b4160f45caf5abecd8e48
Alex <alex@unixfy.net>
2022-07-16 18:54:34 +0000

    Implement navbar & partial darkmode

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+	import { darkmode } from '../stores';
+	import { browser } from '$app/env';
+
+	let mobilemenu = false;
+
+	let toggleDarkMode = function () {
+		$darkmode = !$darkmode;
+	};
+
+	let toggleMobileMenu = function () {
+		mobilemenu = !mobilemenu;
+	};
+
+	onMount(async () => {
+		if (browser) {
+			// Enable darkmode automatically if it's saved in localstorage
+			if ($darkmode) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark');
+			}
+		}
+	});
+</script>
+
+<div class="text-xl font-display font-light">
+	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">Home</span>
+	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx=2">Projects</span>
+	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">LinkedIn</span>
+	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-4 ml-2" on:click="{toggleDarkMode}"
+		><i class="fa-solid fa-sun" /></span
+	>
+</div>
---
+++ src/stores.js
@@ -0,0 +1,53 @@
+// Whether darkmode is enabled or not
+// Enable by default if darkmode is set to true in localstorage or the system scheme is set to dark
+// Persists to localstorage
+
+import { writable } from "svelte/store";
+import { browser } from "$app/env";
+
+const darkmodecalculator = function () {
+    const store = writable(false)
+    // Return false if we aren't running in a browser
+    if (!(browser)) return store;
+
+    // Set the starting value of darkmode
+    let storedvalue
+
+    if (!(browser)) {
+        // If we are in SSR, just set to false
+        storedvalue = false
+    } else if
+    ((!('darkmode' in localStorage) &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        // If the system color scheme is set to dark, then enable darkmode
+        storedvalue = true
+    } else {
+        // If this isn't the case, then try to get the value of darkmode from localstorage and parse it as JSON
+        // Any error occurs, disable darkmode
+        try {
+            storedvalue = JSON.parse(localStorage.getItem("darkmode"))
+        } catch {
+            storedvalue = false
+        }
+    }
+
+    // Now set the store value based on the above
+    if ([true, false].includes(storedvalue)) store.set(storedvalue)
+
+    // Save the darkmode value to localstorage if possible
+    store.subscribe(function (value) {
+        if (browser) {
+            localStorage.setItem("darkmode", value.toString())
+
+            if (value === true) {
+                document.documentElement.classList.add('dark');
+            } else if (value === false) {
+                document.documentElement.classList.remove('dark');
+            }
+        }
+    })
+
+    return store
+}
+
+export const darkmode = darkmodecalculator();
