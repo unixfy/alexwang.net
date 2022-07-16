97c336ac61d0c39d0a2d64d2461bc537612dc127
Alex <alex@unixfy.net>
2022-07-16 20:23:42 +0000

    Make navbar darkmode friendly

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -26,14 +26,14 @@
 </script>
 
 <div class="text-xl font-display font-light my-6 mx-8">
-	<a href="/"><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">Home</span></a>
+	<a href="/"><span class="bg-gray-200 hover:bg-gray-100 dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-white rounded py-2 px-6 mx-2">Home</span></a>
 	<a href="/portfolio"
-		><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx=2">Projects</span></a
+		><span class="bg-gray-200 hover:bg-gray-100 dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-white rounded py-2 px-6 mx=2">Projects</span></a
 	>
 	<a href="/linkedin"
-		><span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-6 mx-2">LinkedIn</span></a
+		><span class="bg-gray-200 hover:bg-gray-100 dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-white rounded py-2 px-6 mx-2">LinkedIn</span></a
 	>
-	<span class="bg-gray-200 hover:bg-gray-100 rounded py-2 px-4 ml-2 hover:cursor-pointer" on:click={toggleDarkMode}
+	<span class="bg-gray-200 hover:bg-gray-100 dark:bg-slate-700 dark:hover:bg-slate-600 dark:text-white rounded py-2 px-4 ml-2 hover:cursor-pointer" on:click={toggleDarkMode}
 		><i class="fa-solid fa-sun" /></span
 	>
 </div>
