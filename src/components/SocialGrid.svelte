b8cf900b89ffba297aeb9f881cba60ba7459dc0d
Alex <alex@unixfy.net>
2022-07-16 19:55:05 +0000

    Make social tiles properly fill the whole space

--- src/components/SocialGrid.svelte
+++ src/components/SocialGrid.svelte
@@ -6,7 +6,7 @@
 <div class="grid grid-cols-3 grid-rows-3">
 	{#each socials as social}
 		<a href={social.url} target="_blank" rel="noopener">
-			<div class="{social.color} flex hover:scale-110 transition hover:drop-shadow-lg">
+			<div class="{social.color} flex hover:scale-110 transition hover:drop-shadow-lg h-full w-full">
 				<span class="text-white m-auto p-10"><i class="fa-brands {social.icon} fa-3x" /></span>
 			</div>
 		</a>
