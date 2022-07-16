5f5bb7873a1391a23cfb637f0b5e1bc87c887003
Alex <alex@unixfy.net>
2022-07-16 20:17:32 +0000

    Make footer darkmode friendly

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="py-8 space-y-8 flex flex-col justify-center content-center">
-		<img alt="Alex Wang signature" src="/signature.svg" class="h-20" />
+<div class="py-8 space-y-8 flex flex-col justify-center content-center dark:bg-slate-900 dark:text-white">
+		<img alt="Alex Wang signature" src="/signature.svg" class="h-20 dark:invert" />
 		<p class="text-center">Copyright &copy; 2020-{new Date().getFullYear()} Alex Wang</p>
 </div>
