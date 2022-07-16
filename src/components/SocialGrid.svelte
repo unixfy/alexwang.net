34d35640033754bdf82c25c311017b333e2a182d
Alex <alex@unixfy.net>
2022-07-16 20:01:12 +0000

    Improve accessibility of social grid

--- src/components/SocialGrid.svelte
+++ src/components/SocialGrid.svelte
@@ -5,7 +5,7 @@
 
 <div class="grid grid-cols-3 grid-rows-3">
 	{#each socials as social}
-		<a href={social.url} target="_blank" rel="noopener">
+		<a href={social.url} target="_blank" rel="noopener" aria-label={social.name}>
 			<div class="{social.color} flex hover:scale-110 transition hover:drop-shadow-lg h-full w-full">
 				<span class="text-white m-auto p-10"><i class="fa-brands {social.icon} fa-3x" /></span>
 			</div>
