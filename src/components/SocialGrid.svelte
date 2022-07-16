2d09b752b86b053221f748370f55609b6e876d66
Alex <alex@unixfy.net>
2022-07-16 19:47:02 +0000

    Load socials data

---
+++ src/components/SocialGrid.svelte
@@ -0,0 +1,14 @@
+<script>
+	// import the color/icon/url data from socials.json
+	import socials from './socials.json';
+</script>
+
+<div class="grid grid-cols-3 grid-rows-3">
+	{#each socials as social}
+		<a href={social.url} target="_blank" rel="noopener">
+			<div class="bg-[{social.color}] flex hover:scale-110 transition hover:drop-shadow-lg">
+				<span class="text-white m-auto p-10"><i class="fa-brands {social.icon} fa-3x" /></span>
+			</div>
+		</a>
+	{/each}
+</div>
