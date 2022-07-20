091d2172b032dd38b706f1ab6060c8e0bd18d177
Alex <alex@unixfy.net>
2022-07-20 01:34:27 +0000

    Improve responsiveness of blog post lister

--- src/components/BlogLister.svelte
+++ src/components/BlogLister.svelte
@@ -7,20 +7,22 @@
 		<a href="#">
 			<div class="flex justify-center">
 				<div
-					class="flex flex-col md:flex-row justify-between transition-all bg-gray-200 rounded-lg border shadow-md md:max-h-48 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+					class="flex flex-col md:flex-row justify-between transition-all bg-gray-200 rounded-lg shadow-md md:max-h-48 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
 				>
 					<img
-						class="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg dark:brightness-75"
+						class="w-full max-h-36 md:h-full object-cover md:w-1/3 md:max-h-full rounded-t-lg md:rounded-none md:rounded-l-lg dark:brightness-75"
 						src="https://images.unsplash.com/photo-1656078249583-c73346944f44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
 						alt=""
 					/>
 					<div class="p-4 flex flex-col justify-start">
-						<p class="mb-2 text-2xl font-bold font-display text-gray-900 dark:text-white capitalize">
-							Title goes here <i class="fa-solid fa-arrow-right" />
+						<p
+							class="mb-2 text-lg md:text-xl lg:text-2xl font-bold font-display text-gray-900 dark:text-white capitalize"
+						>
+							:orem ipsum dolor sit amet <i class="fa-solid fa-arrow-right" />
 						</p>
-						<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-							Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-							chronological order.
+						<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 overflow-hidden">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in quam hendrerit,
+							pellentesque nisi at, pretium velit. Integer sed urna ex.
 						</p>
 					</div>
 				</div>
