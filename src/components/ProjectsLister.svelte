6f31530465b4727dd16652c5157cfe17c4a35909
Alex <alex@unixfy.net>
2022-07-20 04:01:10 +0000

    Improve project lister cards UI

---
+++ src/components/ProjectsLister.svelte
@@ -0,0 +1,31 @@
+<div
+	id="scrollContainer"
+	class="flex flex-no-wrap overflow-x-scroll scroll-smooth cursor-[grab] scrolling-touch items-start mb-8"
+>
+	{#each Array(6) as item}
+		<div
+			class="flex-none w-2/3 md:w-1/3 mr-8 drop-shadow rounded-lg bg-white text-black dark:text-white dark:bg-slate-900"
+		>
+			<a href="#">
+				<div class="flex flex-col">
+					<img
+						class="object-cover rounded-t-lg max-h-1/2"
+						src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+						alt=""
+					/>
+					<div class="p-4">
+						<div class="text-lg md:text-xl lg:text-2xl mb-2 font-bold font-display capitalize ">
+							<p>Some title goes here</p>
+						</div>
+						<div class="text-lg">
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad recusandae, consequatur
+								corrupti vel quisquam id itaque nam
+							</p>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+	{/each}
+</div>
