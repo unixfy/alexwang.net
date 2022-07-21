144e33078d7f0a0e1fa4cb893473f5f62e9e8534
Alex <alex@unixfy.net>
2022-07-21 22:09:16 +0000

    Add page header to portfolio page

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,1 +1,6 @@
-
+<div class="bg-gray-100 dark:bg-gray-800 dark:text-white text-center min-h-[33vh] flex">
+	<div class="m-auto p-16">
+		<h1 class="text-4xl font-display font-bold my-4">Portfolio</h1>
+		<h2 class="text-2xl">Some cool things I've made / done</h2>
+	</div>
+</div>
