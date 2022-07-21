dc9a3e14e5191034cc670b59b8235fb9e29163ce
Alex <alex@unixfy.net>
2022-07-21 21:45:52 +0000

    Improve error message appearance

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -2,10 +2,10 @@
     export let message;
 </script>
 
-<div class="bg-red-500 text-white dark:bg-red-800">
-    <span class="text-xl font-display font-bold">Oops, an error occurred!</span>
-    <span class="font-mono">
+<div class="bg-red-500 text-white dark:bg-red-800 rounded-lg p-4">
+    <p class="text-xl font-display font-bold">Oops, an error occurred!</p>
+    <div class="font-mono my-4 mx-2">
         {message}
-    </span>
-    <span class="text-lg">You can try reloading the page.</span>
+    </div>
+    <p class="text-lg">You can try reloading the page.</p>
 </div>
