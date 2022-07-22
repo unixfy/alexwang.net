ffa35b3a14d6227660ccb5070417cd36c61502b8
Alex <alex@unixfy.net>
2022-07-22 20:21:37 +0000

    Add workaround for DayJS issue

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,11 @@ const config = {
         }
     },
     legacy: { buildSsrCjsExternalHeuristics: true },
+    ssr: {
+        // We have to add this workaround because of #4504 https://github.com/sveltejs/kit/issues/4504
+        // In the future DayJS 2.0 will not require this workaround
+        noExternal: ['dayjs']
+    }
 };
 
 export default config;
