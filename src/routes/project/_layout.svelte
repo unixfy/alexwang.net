015da47c316d616e2ee7f8fb3db349672e0349e8
Alex <alex@unixfy.net>
2022-07-22 03:53:55 +0000

    Add return button to project layout

--- src/routes/project/_layout.svelte
+++ src/routes/project/_layout.svelte
@@ -6,6 +6,7 @@
 <div class="bg-gray-100 dark:bg-gray-800 dark:text-white min-h-[33vh] flex text-center">
 	<div class="m-auto p-16">
 		<h1 class="text-4xl font-display font-bold my-4">Project: {title}</h1>
+		<p class="text-xl text-blue-500 hover:text-blue-300 transition-all"><a href="/portfolio"><i class="fas fa-arrow-left" /> Return to all projects</a></p>
 	</div>
 </div>
 
