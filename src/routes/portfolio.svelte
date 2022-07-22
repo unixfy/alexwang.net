8cbe9f1aeeb81c507c1a51f04218a1b4b1ad268e
Alex <alex@unixfy.net>
2022-07-22 00:58:58 +0000

    Fill portfolio page with content

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,6 +1,14 @@
+<script>
+	import ProjectsLister from '$components/ProjectsLister.svelte';
+</script>
+
 <div class="bg-gray-100 dark:bg-gray-800 dark:text-white text-center min-h-[33vh] flex">
 	<div class="m-auto p-16">
 		<h1 class="text-4xl font-display font-bold my-4">Portfolio</h1>
 		<h2 class="text-2xl">Some cool things I've done</h2>
 	</div>
+</div>
+
+<div class="p-16 bg-[#7E1F86]">
+	<ProjectsLister limit="12" />
 </div>
