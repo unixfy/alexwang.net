b2cf41cb13a4ba2f64e41a14693860286d3eabb1
Alex <alex@unixfy.net>
2022-07-22 23:02:53 +0000

    Remove debugging code from __error

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,17 +1,13 @@
 <script>
-    import { page } from "$app/stores";
-import { onMount } from "svelte";
-
-    onMount(() => {
-        console.log($page)
-    })
+	import { page } from '$app/stores';
 </script>
+
 <div class="min-h-screen dark:bg-black dark:text-white flex text-center">
-<div class="m-auto space-y-4">
-    <h1 class="text-2xl sm:text-3xl font-display font-bold">Oops! An error occurred.</h1>
-    <div class="text-xl sm:text-2xl">
-        {$page.status} - {$page.error.message}
-    </div>
-    <p class="mt-8">You can try refreshing the page or going back home.</p>
-</div>
+	<div class="m-auto space-y-4">
+		<h1 class="text-2xl sm:text-3xl font-display font-bold">Oops! An error occurred.</h1>
+		<div class="text-xl sm:text-2xl">
+			{$page.status} - {$page.error.message}
+		</div>
+		<p class="mt-8">You can try refreshing the page or going back home.</p>
+	</div>
 </div>
