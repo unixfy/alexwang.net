26f82c4cb6eee8c373424d534f4187f5f3c74049
Alex <alex@unixfy.net>
2022-07-24 21:58:21 +0000

    Add links page

---
+++ src/routes/links.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { browser } from '$app/env';
+    import { title } from "../stores";
+    import {page} from "$app/stores";
+	import SocialGrid from '$components/SocialGrid.svelte';
+
+	export let links;
+    
+    $title = "Links"
+</script>
+
+<div class="bg-black text-white p-8 pt-20 flex flex-col">
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<img src="/profile.jpg" class="h-32 w-32 rounded-full m-auto" alt="Account profile photo" />
+	<h1 class="font-display text-2xl sm:text-3xl mt-4 m-auto">@alex.wang.64</h1>
+</div>
+
+<div class="bg-neutral-50 dark:bg-slate-800 dark:text-white sm:pb-8">
+	<div class="max-w-screen-sm mx-auto px-4 py-6 space-y-4 flex flex-col">
+		{#each links as link}
+			<a href={link.url} target="_blank" rel="noopener">
+				<div
+					class="text-center text-lg sm:text-xl bg-sky-500 text-white hover:bg-black transition-all rounded-full py-3"
+				>
+					{#if link.icon}
+						<i class="fa-solid {link.icon}" />
+					{/if}
+					{link.name}
+				</div>
+			</a>
+		{/each}
+	</div>
+	<div class="max-w-screen-sm mx-auto">
+		<SocialGrid />
+	</div>
+</div>
