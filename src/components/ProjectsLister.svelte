839433d7bf9a3cb65d9003c6634a5f5b79289c34
unixfy <alex@unixfy.net>
2023-05-01 22:05:17 -0700

    Patch ProjectsLister for new folder structure

--- src/components/ProjectsLister.svelte
+++ src/components/ProjectsLister.svelte
@@ -7,12 +7,12 @@
 
 	//  note, we use the first n (n = limit) elements of our projects array
 
-	let projects_raw = import.meta.glob('/src/routes/project/*.md', { eager: true });
+	let projects_raw = import.meta.glob('/src/routes/project/**/*.md', { eager: true });
 
 	let projects = Object.entries(projects_raw)
 		.map(([path, data]) => {
-			// this is a REALLY janky way to remove the .md extension and remove the first 2 parts of the path (the /src/routes bit)
-			let slug = '/' + path.replace(/\..+$/, '').split('/').slice(3).join('/');
+			// this is a REALLY janky way to remove the .md extension and remove the first 2 parts of the path (the /src/routes bit) along with the last part (the +page directory)
+			let slug = '/' + path.replace(/\..+$/, '').split('/').slice(3, 5).join('/');
 			// We don't actually need the HTML.. for now
 			// let html = data.default.render()
 			return {
