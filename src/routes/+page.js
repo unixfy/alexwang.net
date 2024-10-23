c7d7b12f6f71fb974e2b5358879d05296424aae2
Alex <alex@unixfy.net>
2024-10-23 23:48:37 +0000

    parallelize Directus requests

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,14 +4,19 @@ import { readItems } from "@directus/sdk";
 export async function load({ fetch }) {
     const directus = getDirectusInstance(fetch);
 
+    const [certifications, socials] = await Promise.all([
+        directus.request(readItems('certifications', { fields: ["name", "reference", "date", "image", "url"], sort: "sort" })),
+        directus.request(readItems('socials', { fields: ["name", "color", "url", "icon"], sort: "sort", limit: 9 }))
+    ]);
+
     return {
         // see this resource: https://svelte.dev/blog/streaming-snapshots-sveltekit
         // we stream the data to the browser as it's loaded so as to not delay pageloads
         streamed: {
             blogPostsRequest: fetch('https://blog.alexwang.net/wp-json/wp/v2/posts?per_page=4')
         },
-        certifications: await directus.request(readItems('certifications', { fields: ["name", "reference", "date", "image", "url"], sort: "sort" })),
-        socials: await directus.request(readItems('socials', { fields: ["name", "color", "url", "icon"], sort: "sort", limit: 9 })),
+        certifications,
+        socials,
         title: "Home"
     }
 }
--- src/routes/links/+page.js
+++ src/routes/links/+page.js
@@ -3,9 +3,15 @@ import { readItems } from '@directus/sdk';
 
 export async function load({ fetch }) {
     const directus = getDirectusInstance(fetch);
+
+    const [links, socials] = await Promise.all([
+        directus.request(readItems('links', { fields: ["title", "url", "icon"], sort: "sort" })),
+        directus.request(readItems('socials', { fields: ["name", "color", "url", "icon"], sort: "sort", limit: 9 }))
+    ]);
+
     return {
-        links: await directus.request(readItems('links', { fields: ["title", "url", "icon"], sort: "sort" })),
-        socials: await directus.request(readItems('socials', { fields: ["name", "color", "url", "icon"], sort: "sort", limit: 9 })),
+        links,
+        socials,
         title: "Links"
     }
 }
