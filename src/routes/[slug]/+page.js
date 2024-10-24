fd1e7acf6f3b5b07ab50b28fe3d6d31a9143efaf
Alex <alex@unixfy.net>
2024-10-24 05:25:58 +0000

    Implement generic page handler from Directus data, remove now page

---
+++ src/routes/[slug]/+page.js
@@ -0,0 +1,20 @@
+import getDirectusInstance from "$lib/directus";
+import { readItems } from "@directus/sdk";
+import { error } from "@sveltejs/kit";
+
+export async function load({ fetch, params }) {
+    const directus = getDirectusInstance(fetch);
+
+    const page_req = await directus.request(readItems('pages', { fields: ["title", "subtitle", "content", "slug", "date_updated"], filter: { slug: params.slug }, limit: 1 }));
+
+    if (page_req.length === 0) {
+        error(404, "Page not found in CMS")
+    }
+
+    const page = page_req[0];
+
+    return {
+        page,
+        title: page.title
+    }
+}
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import dayjs from 'dayjs/esm';
+
+	export let data;
+</script>
+
+<div class="bg-gray-100 dark:bg-gray-800 dark:text-white text-center min-h-[33vh] flex">
+	<div class="m-auto p-16">
+		<h1 class="text-4xl font-display font-bold my-4">{data.page.title}</h1>
+
+		{#if data.page.subtitle}
+			<h2 class="text-2xl">
+				{@html data.page.subtitle}
+			</h2>
+		{/if}
+
+		<p class="mt-2 italic">Last updated {dayjs(data.page.date_updated).format('MMMM DD, YYYY')}</p>
+	</div>
+</div>
+
+<div class="dark:bg-gray-900 dark:text-white min-h-[80vh]">
+    <div class="p-8 lg:p-16 max-w-screen-lg m-auto">
+        <div class="flex flex-col space-y-4 text-lg page-content-container">
+            {@html JSON.stringify(data.page.content)}
+        </div>
+    </div>
+</div>
